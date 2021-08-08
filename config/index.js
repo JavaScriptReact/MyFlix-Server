f499bd4eb876c4b5fbb2c5c0fdd984c374cf6cd1
LucasCoder1 <lucas.developer.dm@gmail.com>
2021-08-08 22:40:12 +0200

    update

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  PORT: 4000,
+  PORT: process.env.PORT || 4000,
   COOKIE_SECRET: process.env.COOKIE_SECRET,
   MONGODB_SECRET: process.env.MONGODB_SECRET,
   JWT_SECRET: process.env.JWT_SECRET,
